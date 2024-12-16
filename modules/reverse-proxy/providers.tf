7b6668cd05f299d9c6ef501255b3be3670264f2c
Charles S <git@cfstcyr.com>
2024-12-16 22:56:05 +0100

    remove unused provider

--- modules/reverse-proxy/providers.tf
+++ modules/reverse-proxy/providers.tf
@@ -1,10 +1,5 @@
 terraform {
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.5.2"
-    }
-    
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.33.0"
